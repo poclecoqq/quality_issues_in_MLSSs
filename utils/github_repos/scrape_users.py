7e497ac50eac01625974a0f1260294ce8253d6fd
Pierre-Olivier Côté <pierre-olivier.cote@polymtl.ca>
2022-10-07 14:08:47 -0400

    fetches all user ids for a user search

--- utils/github_repos/scrape_users.py
+++ utils/github_repos/scrape_users.py
@@ -1,14 +1,60 @@
 import datetime
+import pickle
+from tqdm import tqdm
+
 from github_queries.user import get_n_results_github_user_search, search_users_github
 
+
+def fetch_all_users_ids(tag):
+    """
+    Fetches all users matching some keyword (i.e. tag).
+    Input:
+        tag: The keyword used to perform the search
+    Output:
+        user_ids
+    """
+    def fetch_all_users_ids_rec(tag, start_date, end_date):
+        """
+        The recursive function for fetch_all_users_ids function.
+        Divides the time period by 2 if there is too much results.
+        Output:
+            user_ids
+        """
+        n_results = get_n_results_github_user_search(
+            tag, start_date.isoformat(), end_date.isoformat())
+        GITHUB_MAX_RESULTS = 1000
+        if n_results >= GITHUB_MAX_RESULTS:
+            mid_date = start_date + (end_date-start_date)/2
+            user_ids_l = fetch_all_users_ids_rec(tag, start_date, mid_date)
+            user_ids_r = fetch_all_users_ids_rec(tag, mid_date, end_date)
+            return user_ids_l + user_ids_r
+        else:
+            user_ids = []
+            page_n = 1
+            while len(user_ids) < n_results:
+                user_ids += search_users_github(tag, start_date.isoformat(),
+                                                end_date.isoformat(), page_n)
+                page_n += 1
+
+            nonlocal progress
+            progress += len(user_ids)
+            print(f'{progress}/{tot_n_results}')
+            return user_ids
+
+    # Github cration date (-1 day)
+    start_date = datetime.date(2008, 10, 18)
+    end_date = datetime.datetime.now().date()
+    progress = 0
+    tot_n_results = get_n_results_github_user_search(
+        tag, start_date.isoformat(), end_date.isoformat())
+
+    print(
+        f'---------------- User search with keyword "{tag}" ----------------')
+    return fetch_all_users_ids_rec(tag, start_date, end_date)
+
+
 if __name__ == "__main__":
-    now = datetime.datetime.now().isoformat()
-    # Actually, GitHub was created the next day..
-    GITHUB_CREATION_DATE = datetime.date(2008, 10, 18).isoformat()
-    a = get_n_results_github_user_search(
-        "Machine Learning Engineer", GITHUB_CREATION_DATE, now)
+    a = fetch_all_users_ids("Machine Learning Engineer")
     print(a)
-    a = search_users_github(
-        "Machine Learning Engineer", GITHUB_CREATION_DATE, now, 1)
-    print(a)
-    print(len(a))
+    with open("tmp.bin", 'wb') as f:
+        pickle.dump(a, f)
