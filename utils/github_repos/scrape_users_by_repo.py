8508cff766956b85c4f4f97a46efd141a748d6af
Pierre-Olivier Côté <pierre-olivier.cote@polymtl.ca>
2023-03-25 17:40:18 -0400

    all demographic plots done

---
+++ utils/github_repos/scrape_users_by_repo.py
@@ -0,0 +1,11 @@
+
+
+# TODO: list contributor docs: https://docs.github.com/en/rest/repos/repos#list-repository-contributors
+
+def main(topics):
+    pass
+
+
+if __name__ == "__main__":
+    topics = ['Data Scientist']
+    main(topics)
